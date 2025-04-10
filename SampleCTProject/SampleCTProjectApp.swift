3c863735767b0c7c2c51be137f117e638433efb8
Max Margolis <max@clevertap.com>
2025-04-10 09:38:11 -0700

    Initial commit with gitignore

---
+++ SampleCTProject/SampleCTProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCTProjectApp.swift
+//  SampleCTProject
+//
+//  Created by Max Balsam  Margolis on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleCTProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleCTProjectTests/SampleCTProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  SampleCTProjectTests.swift
+//  SampleCTProjectTests
+//
+//  Created by Max Balsam  Margolis on 4/10/25.
+//
+
+import Testing
+
+struct SampleCTProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
