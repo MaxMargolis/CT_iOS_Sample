2e820838bae4599a82f21d5e1d8e3c5d4f2e7879
Max Margolis <max@clevertap.com>
2025-04-23 17:24:03 -0700

    Setup App Inbox

--- SampleCTProject/ContentView.swift
+++ SampleCTProject/ContentView.swift
@@ -10,18 +10,27 @@ import CleverTapSDK
 
 struct ContentView: View {
     @State private var loggedInUser: String? = nil
+    @State private var viewDidLoad = false
     var body: some View {
-        VStack {
-            Text("Logged in User: \(loggedInUser ?? "No user logged in")")
-            Spacer()
-            Button("Log in Max Margolis") {
-                print("Max Margolis has been logged in")
-                loggedInUser = "Max Margolis"
-                logInMaxMargolis()
+        NavigationView {
+            VStack(spacing:100) {
+                Text("Logged in User: \(loggedInUser ?? "No user logged in")")
+                Button("Log in Max Margolis") {
+                    print("Max Margolis has been logged in")
+                    loggedInUser = "Max Margolis"
+                    logInMaxMargolis()
+                }
+                NavigationLink("Go To App Inbox") {
+                    CTAppInboxRepresentable()
+                }
             }
-            Spacer()
+            .onAppear() {
+                if viewDidLoad == false {
+                    viewDidLoad = true
+                    initializeAppInbox()
+                }
+            }
         }
-        .padding()
     }
 }
 
@@ -31,6 +40,14 @@ private extension ContentView {
                                      "Name": "Max Margolis",
                                      "Identity": "MaxMargolis"] as [AnyHashable : Any]
         CleverTap.sharedInstance()?.onUserLogin(maxMargolisProperties)
+    }
+    
+    func initializeAppInbox() {
+        CleverTap.sharedInstance()?.initializeInbox(callback: ({ (success) in
+                let messageCount = CleverTap.sharedInstance()?.getInboxMessageCount()
+                let unreadCount = CleverTap.sharedInstance()?.getInboxMessageUnreadCount()
+                print("Inbox Message:\(String(describing: messageCount))/\(String(describing: unreadCount)) unread")
+         }))
     }
 }
 
