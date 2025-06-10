067e67b41a17645b489f362b83b871ee231fdd94
Max Margolis <max@clevertap.com>
2025-06-10 15:52:21 -0700

    Start Atlantis

--- SampleCTProject/SampleCTProjectApp.swift
+++ SampleCTProject/SampleCTProjectApp.swift
@@ -5,10 +5,19 @@
 //  Created by Max Balsam  Margolis on 4/10/25.
 //
 import SwiftUI
+#if DEBUG
+import Atlantis
+#endif
 
 @main
 struct SampleCTProjectApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    init() {
+        #if DEBUG
+        Atlantis.start()
+        #endif
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
